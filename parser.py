07f722326b6e6e1882f508c1f9e81fa813e83d8b
Stephan Doan <sdoan@stuy.edu>
2017-03-06 11:42:33 -0500

    start

---
+++ parser.py
@@ -0,0 +1,48 @@
+from display import *
+from matrix import *
+from draw import *
+
+"""
+Goes through the file named filename and performs all of the actions listed in that file.
+The file follows the following format:
+     Every command is a single character that takes up a line
+     Any command that requires arguments must have those arguments in the second line.
+     The commands are as follows:
+         line: add a line to the edge matrix - 
+	    takes 6 arguemnts (x0, y0, z0, x1, y1, z1)
+	 ident: set the transform matrix to the identity matrix - 
+	 scale: create a scale matrix, 
+	    then multiply the transform matrix by the scale matrix - 
+	    takes 3 arguments (sx, sy, sz)
+	 translate: create a translation matrix, 
+	    then multiply the transform matrix by the translation matrix - 
+	    takes 3 arguments (tx, ty, tz)
+	 rotate: create a rotation matrix,
+	    then multiply the transform matrix by the rotation matrix -
+	    takes 2 arguments (axis, theta) axis should be x, y or z
+	 apply: apply the current transformation matrix to the 
+	    edge matrix
+	 display: draw the lines of the edge matrix to the screen
+	    display the screen
+	 save: draw the lines of the edge matrix to the screen
+	    save the screen to a file -
+	    takes 1 argument (file name)
+	 quit: end parsing
+
+See the file script for an example of the file format
+"""
+def parse_file( fname, points, transform, screen, color ):
+    comm = []
+    f = open(fname):
+    comm = f.readlines()
+    while (i < range(len(comm))):
+        comm[i] = comm[i].strip('\n')
+        if (comm[i] == 'line'):
+            i++
+            args=[] = comm[i].split()
+            add_edge(points, args[0], args[1], args[2], args[3], args[4], args[5])
+        elif (comm[i] == 'ident'):
+            ident(transform)
+
+        elif (comm[i] == 'scale')
+        pass
